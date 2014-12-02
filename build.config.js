355ca163fd7d145f7c69ff1a02ab4e17e5e80a84
CrouseMatthew <mscrouse@gmail.com>
2014-12-01 17:24:38 -0800

    added lightbox information

--- build.config.js
+++ build.config.js
@@ -66,11 +66,18 @@ module.exports = {
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui-utils/modules/route/route.js'
+      'vendor/angular-ui-utils/modules/route/route.js',
+      'vendor/jquery/dist/jquery.js',
+      'vendor/angular-touch/angular-touch.js',
+      'vendor/angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.js',
+      'vendor/magnific-popup/dist/jquery.magnific-popup.js'
     ],
     css: [
+    'vendor/magnific-popup/dist/magnific-popup.css',
+    'vendor/angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.css'
     ],
     assets: [
+      'assets/myCss.css'
     ]
   },
 };
